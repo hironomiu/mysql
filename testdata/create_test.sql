1d84617ab4d8781a4473f50b34306245cd6897fe
hironomiu <h-miura@www2270uf.sakura.ne.jp>
2012-05-04 21:32:22 +0900

    create table test

---
+++ testdata/create_test.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `test` (
+	  `id` int(11) NOT NULL AUTO_INCREMENT,
+	  `other_id` int(11) NOT NULL,
+	  `col1` int(11) DEFAULT NULL,
+	  `col2` int(11) DEFAULT NULL,
+	  `col3` varchar(15) DEFAULT NULL,
+	  `col4` varchar(255) DEFAULT NULL,
+	  `col5` varchar(255) DEFAULT NULL,
+	  `col6` varchar(255) DEFAULT NULL,
+	  `col7` varchar(255) DEFAULT NULL,
+	  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
